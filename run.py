c550d48380da684e3bb47cd0dd33be6faad942e5
enyq <137332627+enyq0371@users.noreply.github.com>
2023-06-28 23:53:32 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+#nft持有即得分
+#1. Mainnet Alpha NFT Holders（10分）
+#2. Loyalty Program NFT Holders（10分）
+#nft跨链任务
+#1.Cobee NFT Cross-chain（25分）：把cobee nft从bnb网络转移到combo网络，具体流程如下图2-4
+#2.Mainnet Alpha NFT Cross-Chain（40分）：把Mainnet Alpha NFT从bnb跨链到任意网络，流程和上面是一样的
+#3. Loyalty Program NFT Cross-Chain（30分）：把Loyalty Program NFT从bnb跨链到任意网络，流程和上面是一样的
+#4. May Greenfield zkMessenger（30分）：进入官网的发信息页面，
+#
+#
+#
+#
+if __name__ == '__main__':
+  pass
